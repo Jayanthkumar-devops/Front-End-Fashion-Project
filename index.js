1b001f7f89d17e5477f8dd683bb44eaffcf85d32
Jayanthkumar-devops <jayanthkumar.krish@gmail.com>
2024-09-19 11:57:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var sidenav = document.querySelector(".side-navbar")
+
+
+function showNavbar(){
+    sidenav.style.left="0"
+}
+function closeNavbar(){
+    sidenav.style.left="-60%"
+}
+
+var img1 = document.querySelectorAll(".img1")
+
+function openpage(){
+    
+
+}
